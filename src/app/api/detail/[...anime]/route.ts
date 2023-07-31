068fe145ff1f7173f89e68724025c5f2ad183991
Ahmad Shaleh Kurniawan <ahmadsaleh2409@gmail.com>
2023-07-31 16:08:20 +0700

    update type detail anime

--- src/app/api/detail/[...anime]/route.ts
+++ src/app/api/detail/[...anime]/route.ts
@@ -1,12 +1,40 @@
 import {
   responseErrorWithMessage,
   responseSuccessWithData,
-  responseSuccessWithMessage,
 } from "@/lib/Response";
 import SiteConfig from "@/lib/SiteConfig";
 import { NextResponse } from "next/server";
 const cheerio = require("cheerio");
 const baseURL = SiteConfig.scrapUrl;
+
+type EpsType = { episodeId: any; epsTitle: any };
+
+interface IDetailAnime {
+  type: string;
+  title: string;
+  englishTitle: string;
+  synopsis: string;
+  status: string;
+  image: string;
+  ratings: string;
+  animeQuality: string;
+  totalEps: string;
+  aired: string;
+  season: string;
+  duration: string;
+  country: string;
+  adaptation: string;
+  genres: string;
+  explisit: string;
+  demografis: string;
+  theme: string;
+  skors: string;
+  studio: string;
+  peminat: string;
+  ratingText: string;
+  credit: string;
+  episode: Array<EpsType>;
+}
 
 export async function GET(
   req: Request,
@@ -19,44 +47,17 @@ export async function GET(
     const html = await rawResponse.text();
     const $ = cheerio.load(html);
 
-    const el = $(
-      "body > section > div > div.anime__details__content"
-    );
+    const el = $("body > section > div > div.anime__details__content");
 
-    let datas: {
-      type: string;
-      title: string;
-      englishTitle: string;
-      synopsis: string;
-      status: string;
-      image: string;
-      ratings: string;
-      animeQuality: string;
-      totalEps: string;
-      aired: string;
-      season: string;
-      duration: string;
-      country: string;
-      adaptation: string;
-      genres: string;
-      explisit: string;
-      demografis: string;
-      theme: string;
-      skors: string;
-      studio: string;
-      peminat: string;
-      ratingText: string;
-      credit: string;
-      episode: { episodeId: string; epsTitle: string }[];
-    }[] = [];
+    let datas: Array<IDetailAnime> = [];
 
     let episode = $("#episodeLists").attr("data-content");
     const $$ = cheerio.load(episode);
 
-    let episodeArray: { episodeId: any; epsTitle: any }[] = [];
+    let episodeArray: Array<EpsType> = [];
     $$("a").each((i: any, e: any) => {
       const eps = $(e).attr("href").trim().replace(`${baseURL}`, "");
-      const epsTitle = $(e).text().replace(/\s+/g, " ");
+      const epsTitle = $(e).text().trim().replace(/\s+/g, " ");
       episodeArray.push({
         episodeId: eps,
         epsTitle: epsTitle,
@@ -179,12 +180,13 @@ export async function GET(
             "div.col-lg-9 > div > div.anime__details__widget > div > div:nth-child(2) > ul > li:nth-child(9) > a"
           )
           .text()
+          .trim()
           .replace(/\s+/g, " "),
         episode: episodeArray,
       })
     );
 
-    return NextResponse.json(responseSuccessWithData(datas));
+    return NextResponse.json(responseSuccessWithData(datas[0]));
   } catch (error) {
     console.log("Detail Error: ", error);
 
